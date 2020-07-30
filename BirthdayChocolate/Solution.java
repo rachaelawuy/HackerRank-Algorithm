0d3e62639203202b384394c672350667d7b0e35d
Rachael Awuy <rachaelawuy@hotmail.com>
2020-07-30 11:20:11 +0900

    Create Solution.java

---
+++ BirthdayChocolate/Solution.java
@@ -0,0 +1,60 @@
+//Problem at https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+        int res=0;
+        if(m==1 && s.size()==1 && s.get(0)==d) {
+            res=1;
+        } else {
+            for(int i=0; i<=(s.size()-m); i++){
+                int temp=s.get(i);
+                int counter=1;
+                while (counter<m){
+                    temp+=s.get(i+counter);
+                    counter+=1;
+            }
+            if(temp==d) res+=1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
